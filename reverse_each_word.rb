e7b8938e41472570a41c8822b471004dad276945
James Mobus <mobusj@live.com>
2019-08-08 14:15:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
 def reverse_each_word(str)
   str_array = str.split(" ")
   
-  str_array.collect {|s|  }
-  str_array.length.times do |i|
-    str_array[i] = str_array[i].reverse
-  end
+  str_array.collect {|s| s.reverse }
   
   str = str_array.join(" ")
   
